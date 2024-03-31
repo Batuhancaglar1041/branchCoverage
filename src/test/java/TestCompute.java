c420c2629a13a530eeeb0f663eb543d8d08dd3d1
Batuhancaglar1041 <123544820+Batuhancaglar1041@users.noreply.github.com>
2024-03-31 23:26:59 +0300

    changed tstt compute

--- src/test/java/TestCompute.java
+++ src/test/java/TestCompute.java
@@ -33,4 +33,27 @@ public class TestCompute {
     assertEquals(0, compute.countNumberOfOccurrences("a"));
   }
 
+  @Test
+  public void testCountNumberOfOccurrences_WithSingleOccurrence() {
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(1);
+    when(mq.contains("a")).thenReturn(true);
+    when(mq.getAt(0)).thenReturn("a");
+    Compute compute = new Compute(mq);
+
+    assertEquals(1, compute.countNumberOfOccurrences("a"));
+  }
+  @Test
+  public void testCountNumberOfOccurrences_WithMultipleOccurrences() {
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(3);
+    when(mq.contains("a")).thenReturn(true);
+    when(mq.getAt(0)).thenReturn("a");
+    when(mq.getAt(1)).thenReturn("b");
+    when(mq.getAt(2)).thenReturn("a");
+    Compute compute = new Compute(mq);
+
+    assertEquals(2, compute.countNumberOfOccurrences("a"));
+  }
+
 }
