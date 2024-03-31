b498a750df24a97821bc569daa299e58b49654c4
Batuhancaglar1041 <123544820+Batuhancaglar1041@users.noreply.github.com>
2024-03-31 23:32:22 +0300

    util test %94 changed

--- src/test/java/TestUtil.java
+++ src/test/java/TestUtil.java
@@ -28,9 +28,13 @@ public class TestUtil {
   @Test
   public void testCompute_WithSumDivisibleByArgument() {
     Util util = new Util();
-    assertTrue(util.compute(1, 3, 5)); // Sum = 9, 9 is divisible by 3
+    assertTrue(util.compute(1, 3, 5)); // Sum = 9, 9 üçe bölünebilir
   }
-
+  @Test(expected = RuntimeException.class)
+  public void testCompute_WithZeroElementInArguments_ThrowsException() {
+    Util util = new Util();
+    util.compute(1, 0, 2); // 0 argümanı içeren durumu test ediyoruz
+  }
 
 
 
